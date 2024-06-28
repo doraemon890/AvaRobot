22f6cf8e1ed51e79fb794fc17a31fd2bb60c6f82
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-06-28 11:13:44 -0700

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -26,5 +26,4 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it
-net stop Ava
-net start Ava
+nssm restart Ava
